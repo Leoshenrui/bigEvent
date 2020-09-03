5a3159aaa4a2488ef77796ca553b2f4cbdeaa001
leoshen <490335922@qq.com>
2020-09-03 14:41:29 +0800

    index1

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -6,4 +6,22 @@ $.ajaxPrefilter(function(options) {
     console.log(options);
     options.url = 'http://ajax.frontend.itheima.net' + options.url;
 
+    // 统一为有权限的接口，设置headers请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    };
+
+    // 当前请求结束后：判断用户的设置访问权限
+    options.complete = function(res) {
+        console.log(res);
+        // 当用户身份认证失败
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 1.强制清空本地token
+            localStorage.removeItem('token');
+            // 跳转到登入界面
+            location.href = '/bigEvent/login.html';
+        }
+    }
 })
