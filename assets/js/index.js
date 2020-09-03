d927a4d1250be86b267155860f3861228568ee86
leoshen <490335922@qq.com>
2020-09-03 20:36:37 +0800

    一一

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,48 +1,49 @@
+// 调用 getUserInfo 获取用户基本信息
+getUserInfo()
+
+// 获取用户的基本信息
+function getUserInfo() {
+    $.ajax({
+        url: '/my/userinfo',
+        type: 'GET',
+        // // headers就是请求头配置对象
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function(res) {
+            console.log(res);
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败')
+            };
+            // 渲染头像
+            renderAvatar(res.data)
+        },
+        // success(res) {
+        //     console.log(res);
+        // }
+    })
+};
+
+function renderAvatar(user) {
+    // 1.获取 昵称 或 用户名
+    var name = user.nickname || user.username;
+    // 2.欢迎文本
+    $('#welcome').html(`欢迎&nbsp;&nbsp;${name}`);
+    // 3.按需渲染用户头像
+    if (user.user_pic !== null) {
+        // 3.1渲染图片头像，并显示头像
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+        $('.text-avatar').hide() //隐藏文字头像
+    } else {
+        // 3.2渲染文字头像
+        $('.layui-nav-img').hide(); //隐藏图片头像
+        var first = name[0].toUpperCase(); //获取用户名第一个首字母并设置大写
+        // 显示文字头像的显示内容，并显示 
+        $('.text-avatar').html(first).show()
+    }
+};
 $(function() {
-    // 调用 getUserInfo 获取用户基本信息
-    getUserInfo()
 
-    // 获取用户的基本信息
-    function getUserInfo() {
-        $.ajax({
-            url: '/my/userinfo',
-            type: 'GET',
-            // // headers就是请求头配置对象
-            // headers: {
-            //     Authorization: localStorage.getItem('token') || ''
-            // },
-            success: function(res) {
-                console.log(res);
-                if (res.status !== 0) {
-                    return layui.layer.msg('获取用户信息失败')
-                };
-                // 渲染头像
-                renderAvatar(res.data)
-            },
-            // success(res) {
-            //     console.log(res);
-            // }
-        })
-    };
-
-    function renderAvatar(user) {
-        // 1.获取 昵称 或 用户名
-        var name = user.nickname || user.username;
-        // 2.欢迎文本
-        $('#welcome').html(`欢迎&nbsp;&nbsp;${name}`);
-        // 3.按需渲染用户头像
-        if (user.user_pic !== null) {
-            // 3.1渲染图片头像，并显示头像
-            $('.layui-nav-img').attr('src', user.user_pic).show();
-            $('.text-avatar').hide() //隐藏文字头像
-        } else {
-            // 3.2渲染文字头像
-            $('.layui-nav-img').hide(); //隐藏图片头像
-            var first = name[0].toUpperCase(); //获取用户名第一个首字母并设置大写
-            // 显示文字头像的显示内容，并显示 
-            $('.text-avatar').html(first).show()
-        }
-    };
 
     var layer = layui.layer;
     // 点击按钮，实现退出功能
--- assets/js/user/user_pwd.js
+++ assets/js/user/user_pwd.js
@@ -1,34 +1,58 @@
 $(function() {
-  var form = layui.form
-
-  form.verify({
-    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-    samePwd: function(value) {
-      if (value === $('[name=oldPwd]').val()) {
-        return '新旧密码不能相同！'
-      }
-    },
-    rePwd: function(value) {
-      if (value !== $('[name=newPwd]').val()) {
-        return '两次密码不一致！'
-      }
-    }
-  })
-
-  $('.layui-form').on('submit', function(e) {
-    e.preventDefault()
-    $.ajax({
-      method: 'POST',
-      url: '/my/updatepwd',
-      data: $(this).serialize(),
-      success: function(res) {
-        if (res.status !== 0) {
-          return layui.layer.msg('更新密码失败！')
+    var form = layui.form;
+    form.verify({
+        pass: [
+            /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+        ],
+        samePwd: function(value) {
+            if (value === $('[name=oldPwd]').val()) {
+                return "新旧密码不能相同！"
+            }
+        },
+        rePwd: function(value) {
+            if (value !== $('[name=newPwd]').val()) {
+                return "两次密码不一致！"
+            }
         }
-        layui.layer.msg('更新密码成功！')
-        // 重置表单
-        $('.layui-form')[0].reset()
-      }
+    });
+    $('.layui-form').on('submit', function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: '/my/updatepwd',
+            type: 'POST',
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layui.layer.msg('更新密码失败！');
+                };
+                layui.layer.msg('更新密码成功！');
+                // 将jq对象转换为DOM对象，调用重置表单的放发
+                $('.layui-form')[0].reset();
+            }
+        })
     })
-  })
 })
+
+
+
+
+
+// $(function() {
+
+//   $('.layui-form').on('submit', function(e) {
+//     e.preventDefault()
+//     $.ajax({
+//       method: 'POST',
+//       url: '/my/updatepwd',
+//       data: $(this).serialize(),
+//       success: function(res) {
+//         if (res.status !== 0) {
+//           return layui.layer.msg('更新密码失败！')
+//         }
+//         layui.layer.msg('更新密码成功！')
+//         // 重置表单
+//         $('.layui-form')[0].reset()
+//       }
+//     })
+//   })
+// })
