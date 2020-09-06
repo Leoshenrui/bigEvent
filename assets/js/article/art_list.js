976330b6ca5008823415afedd9474e36240884cd
leoshen <490335922@qq.com>
2020-09-06 17:05:24 +0800

    文章部分完成

--- assets/js/article/art_list.js
+++ assets/js/article/art_list.js
@@ -1,6 +1,8 @@
 // cz 123456
 
 $(function() {
+    var form = layui.form;
+    var laypage = layui.laypage;
 
     // 定义美化时间的过滤器
     template.defaults.imports.dataFormat = function(data) {
@@ -48,10 +50,120 @@ $(function() {
                 // 调用模板引擎渲染分类的可选项
                 var htmlStr = template('tpl-table', res);
                 $('tbody').html(htmlStr);
+                renderPage(res.total)
             }
         })
     }
+
+
+    // 获取文章分类的方法
+    initCate();
+
+    function initCate() {
+        $.ajax({
+            url: '/my/article/cates',
+            type: 'GET',
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取分类数据失败！')
+                };
+                console.log(res);
+                // 调用模板引擎渲染分类的可选项
+                var htmlStr = template('tpl-cate', res);
+                $('[name=cate_id]').html(htmlStr);
+                // 通过 layui 重新渲染表单区域的UI结构
+                form.render()
+            }
+        })
+    };
+
+    // 微筛选表单邦定submit事件
+    $('#form-search').on('submit', function(e) {
+        e.preventDefault();
+        // 获取表单中选中项的值
+        var cate_id = $('[name=cate_id]').val();
+        var state = $('[name=state]').val();
+        // 为查询参数对象q中对应的属性赋值
+        q.cate_id = cate_id;
+        q.state = state;
+        // 根据最新的筛选条件，重新渲染表格数据
+        initTable();
+    });
+
+    // 定义渲染分页的方法
+    function renderPage(total) {
+        // console.log(total);
+        // 调用laypage.render()方法来渲染分页的结构
+        laypage.render({
+            elem: 'pageBox', // 分页容器的 Id
+            count: total, // 总数据条数
+            limit: q.pagesize, // 每页显示几条数
+            curr: q.pagenum, // 设置默认被选中的分页
+            layout: ['count', 'limit', 'prve', 'page', 'next', 'skip'],
+            limits: [2, 3, 5, 10],
+
+            // 分页发生切换的时候，触发 jump 回调
+            jump: function(obj, first) {
+                // 可以通过 first 的值，来判断是通过哪种方式，触发的 jump 回调
+                // 1. 点击页码的时候，会触发 jump 回调
+                // 2. 只要调用了 laypage.render() 方法，就会触发 jump 回调
+                // first为undefined表示手动触发
+                // first为true表示程序触发
+                console.log('jump触发的方式：' + first);
+
+
+                // console.log(obj.curr);
+                // 把最新的页码值，赋值到 q 这个查询参数对象中
+                q.pagenum = obj.curr;
+
+                // 把最新的条目数，赋值到q这个查询参数对象的pagesize属性中
+                q.pagesize = obj.limit
+
+                // 根据最新的q获取对应的数据列表，并渲染表格
+                if (!first) {
+                    initTable();
+                }
+
+            }
+        })
+    };
+
+
+    // 通过代理的形式，为删除按钮绑定点击事件处理函数
+    $('tbody').on('click', '.btn-delete', function() {
+        // 获取删除按钮的个数——当前页面数据的条款
+        var len = $('.btn-delete').length;
+
+        // 获取文章的id
+        var id = $(this).attr('data-id');
+        // 询问用户是否要删除数据
+        layer.confirm('确认删除？', { icon: 3, title: '提示' }, function(index) {
+            $.ajax({
+                url: '/my/article/delete/' + id,
+                type: 'GET',
+                success: function(res) {
+                    if (res.status !== 0) {
+                        return layer.msg('删除文章失败！')
+                    }
+                    layer.msg('删除文章成功！');
+
+                    // 如果当前页只有一条数据，并且当前的页码值不为1
+                    if (len === 1 && q.pagenum !== 1) {
+                        // 如果 len 的值等于1，证明删除完毕之后，页面上就没有任何数据了
+                        // 页码值最小必须是 1
+                        q.pagenum -= 1
+                    }
+                    initTable();
+                }
+            })
+            layer.close(index);
+        });
+
+    })
+
 })
+
+
 
 
 
